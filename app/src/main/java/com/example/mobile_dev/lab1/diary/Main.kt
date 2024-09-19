8087d80c57c895bd300bdbf709313e9ffcecf2fd
krasilnikov30 <bkrasilnikov3006@gmail.com>
2024-09-19 22:09:11 +0300

    Сверстал аватар, доделал лабу 1

--- app/src/main/java/com/example/mobile_dev/lab1/diary/Main.kt
+++ app/src/main/java/com/example/mobile_dev/lab1/diary/Main.kt
@@ -1,12 +1,54 @@
 package com.example.mobile_dev.lab1.diary
 
-fun makeIdGenerator(): () -> Int {
+operator fun <T> List<T>.component2(): List<T> = this.drop(1)
+
+fun makeIdGenerator(): () -> Id {
     var id = 0
-    return { id++ }
+    return { Id(id++.toString()) }
 }
 
-fun main() {
-    val studentInfos = readln().split(',')
-    val generateId = makeIdGenerator()
+val createStudent = { id: Id, params: List<String> ->
+    id to Student(id, Name(params[0]), Age(params[1].toInt()), Score(params[2].toInt()))
+}
 
+val readStudents = { generateId: () -> Id ->
+    readln()
+        .split(", ")
+        .map { it.split(' ') }
+        .associate { createStudent(generateId(), it) }
+        .toMutableMap()
+}
+
+fun updatePoints(id: Id, score: Score, studentMap: StudentMap) {
+    val student = studentMap[id] ?: return
+    studentMap[id] = student.copy(score = score)
+}
+
+fun rename(id: Id, name: Name, studentMap: StudentMap) {
+    val student = studentMap[id] ?: return
+    studentMap[id] = student.copy(name = name)
+}
+
+typealias StudentMap = MutableMap<Id, Student>
+
+fun main() {
+    val generateId = makeIdGenerator()
+    val students: StudentMap = readStudents(generateId)
+
+    do {
+        val (command, params) = readln().split(' ')
+        when (command) {
+            "add" -> students += createStudent(generateId(), params)
+            "remove" -> students -= Id(params[0])
+            "update_points" -> updatePoints(Id(params[0]), Score(params[1].toInt()), students)
+            "rename" -> rename(Id(params[0]), Name(params[1]), students)
+            "print_sort_by_name" -> students.values
+                .sortedBy { it.name.value }
+                .forEach { println(it) }
+
+            "print_sort_by_points" -> students.values
+                .sortedByDescending { it.score.value }
+                .forEach { println(it) }
+        }
+    } while (command != "exit")
 }
--- app/src/main/java/com/example/mobile_dev/lab1/diary/Student.kt
+++ app/src/main/java/com/example/mobile_dev/lab1/diary/Student.kt
@@ -1,12 +1,19 @@
 package com.example.mobile_dev.lab1.diary
 
-class Student(name: String, age: Int, score: Int) {
+data class Id(val value: String)
+data class Name(val value: String)
+
+data class Age(val value: Int)
+data class Score(val value: Int)
+
+data class Student(
+    val id: Id,
+    val name: Name,
+    val age: Age,
+    val score: Score,
+) {
 
     override fun toString(): String {
-        return "$m_name ($m_age лет) - $m_score балла"
+        return "${name.value} (${age.value} лет) - ${score.value} балла"
     }
-
-    private val m_name = name
-    private val m_age = age
-    private val m_score = score
 }
